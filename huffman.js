394c6205808588d039c0fec9e4b86753eb04ed29
Shubham Choudhary <work.shubhamchoudhary@gmail.com>
2021-04-13 17:36:59 +0530

    generating huffman tree

--- huffman.js
+++ huffman.js
@@ -7,7 +7,7 @@ class Huffman {
 		// getting heap from BinaryHeap class
 		this.heap = new BinaryHeap();
 
-		// storing the count of freq of each character of data in map
+		// storing the count of freq of each character of data in hashmap
 		const mp = new Map();
 		for (let i = 0; i < data.length(); i++) {
 			// if character is present in map
@@ -21,7 +21,26 @@ class Huffman {
 		// Insert elements in the heap
 		// push with -ve value as we need min heap
 		for (const key in mp) {
+			// we are not simply pushing a value but an array into heap [frequency, characterKey]
 			this.heap.insert([-mp[key], key]);
 		}
+
+		// Creating HUFFMAN TREE
+		// continuing until the heap has more than 1 element
+		while (this.heap.size() > 1) {
+			// taking out two character from heap with least frequency
+			const node1 = this.heap.extractMax();
+			const node2 = this.heap.extractMax();
+
+			// creating new node with frequency = sum of both nodes pulled out
+			// and the characters are pushed to node too as [leftChild, rightChild]
+			const node = [node1[0] + node2[0], [node1, node2]];
+			// pushing the sum node back into the heap
+			this.heap.insert(node);
+		}
+
+		// Extracting the final node from heap and
+		// it will be the Huffman tree itself
+		const huffman_encoder = this.heap.extractMax();
 	}
 }
